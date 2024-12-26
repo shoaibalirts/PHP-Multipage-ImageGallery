0c3af0df9d84fa765f0d51a9c56d00ffb321b471
Shoaibalirts <shoaibali.rts@gmail.com>
2024-12-26 16:42:57 +0100

    displayed all images as well as generated url paramater as imgae = image.jpg

--- gallery.php
+++ gallery.php
@@ -5,5 +5,11 @@ include './inc/images.inc.php';
 ?>
 <?php include './views/header.php'; ?>
 
+<?php foreach ($imageTitles as $image => $imageTitle) { ?>
+    <div class="card">
+        <h2><?php echo $imageTitle ?></h2>
+        <a href="image=<?php echo $image ?>"><img src="./images/<?php echo rawurldecode($image) ?>" alt="<?php echo $imageTitle ?>" /> </a>
 
+    </div>
+<?php } ?>
 <?php include './views/footer.php'; ?>
